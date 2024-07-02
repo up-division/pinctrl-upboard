630186c34ac9303e3bf0ab93d6a3bdaeb401b11c
GaryWang <garywang@aaeon.com.tw>
2024-07-02 09:29:07 +0800

    improve spi bus small data transfer

--- files/protos.c
+++ files/protos.c
@@ -7,7 +7,7 @@
  * Authors: Gary Wang <garywang@aaeon.com.tw>
  *
  */
-#define CHECK_GPIO
+#define CHECK_SPI_NAMING
 
 #ifdef CHECK_PWM
 #include <linux/pwm.h>
@@ -15,7 +15,7 @@
 static void _check_get_state(struct pwm_chip *chip, struct pwm_device *pwm,
 			       struct pwm_state *state)
 {
-  return;
+    return;
 }
 
 static const struct pwm_ops pwm_lpss_ops = {
@@ -27,8 +27,27 @@ static const struct pwm_ops pwm_lpss_ops = {
 #include <linux/pinctrl/consumer.h>
 void func(void)
 {
-	pinctrl_gpio_request(0);
+    pinctrl_gpio_request(0);
 }
 #endif //CHECK_GPIO
 
+#ifdef CHECK_SPI_NAMING
+#include <linux/spi/pxa2xx_spi.h>
+void foo(void)
+{
+	struct pxa2xx_spi_controller ctrl;
+	ctrl.is_slave=false;
+}
+#endif //CHECK_SPI_NAMING
+ 
+#ifdef CHECK_PLATFORM_DRIVER
+#include <linux/platform_device.h>
+static void platform_remove_new(struct platform_device *pdev)
+{
+    return; 
+}
+static struct platform_driver driver = {
+	.remove_new = platform_remove_new,
+};
+#endif //CHECK_PLATFORM_DRIVER
 
