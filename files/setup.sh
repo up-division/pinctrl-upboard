f4f5722c37e61a630c1087daf02e15f3539d731c
GaryWang <garywang@aaeon.com.tw>
2024-10-23 00:18:37 +0800

    prepare for 1.1.6 release

--- files/setup.sh
+++ files/setup.sh
@@ -14,6 +14,22 @@ cp $SRC_DIR/acpi/spi/*.aml $BASEHOOKSDIR
 #only for ASL user space SPI bus
 rm /lib/firmware/acpi-upgrades/pc00.spi0.spidev1.0.aml
 
+#gpio blacklist check
+if grep -q 'blacklist gpio_aaeon' /etc/modprobe.d/blacklist.conf
+then
+    echo "blacklist gpio_aaeon exist"
+else
+    echo "blacklist gpio_aaeon" >> /etc/modprobe.d/blacklist.conf
+fi
+#spi blacklist check
+if grep -q 'blacklist spi-pxa2xx-platform' /etc/modprobe.d/blacklist.conf
+then
+    echo "blacklist spi-pxa2xx-platform exist"
+else
+    echo "blacklist spi-pxa2xx-platform" >> /etc/modprobe.d/blacklist.conf
+fi
+
+#up boards setting    
 if grep -q 'UPN-APL01' /sys/class/dmi/id/board_name
 then
     cp $SRC_DIR/acpi/spi/pc00.spi0.spidev1.0.aml $BASEHOOKSDIR	
@@ -38,51 +54,20 @@ then
 fi
 if grep -q 'UPX-TGL01' /sys/class/dmi/id/board_name
 then
-    #check blacklist
-    if grep -q 'blacklist gpio_aaeon' /etc/modprobe.d/blacklist.conf
-    then
-        echo "blacklist gpio_aaeon exist"
-    else
-        echo "blacklist gpio_aaeon" >> /etc/modprobe.d/blacklist.conf
-    fi
     rm /lib/firmware/acpi-upgrades/pc00.i2c0.adc0.aml
     rm /lib/firmware/acpi-upgrades/pc00.i2c2.adc0.aml
 fi
 if grep -q 'UPN-EHL01' /sys/class/dmi/id/board_name
 then
-    #check blacklist
-    if grep -q 'blacklist gpio_aaeon' /etc/modprobe.d/blacklist.conf
-    then
-        echo "blacklist gpio_aaeon exist"
-    else
-        echo "blacklist gpio_aaeon" >> /etc/modprobe.d/blacklist.conf
-    fi
     cp $SRC_DIR/acpi/acpi-upgrades /etc/initramfs-tools/hooks/
     cp $SRC_DIR/acpi/gpio/*.aml $BASEHOOKSDIR
     echo 'acpi files copied!'
 fi
 if grep -q 'UPS-EHL01' /sys/class/dmi/id/board_name
 then
-    #check blacklist
-    if grep -q 'blacklist gpio_aaeon' /etc/modprobe.d/blacklist.conf
-    then
-        echo "blacklist gpio_aaeon exist"
-    else
-        echo "blacklist gpio_aaeon" >> /etc/modprobe.d/blacklist.conf
-    fi
     cp $SRC_DIR/acpi/acpi-upgrades /etc/initramfs-tools/hooks/
     cp $SRC_DIR/acpi/gpio/*.aml $BASEHOOKSDIR
     echo 'acpi files copied!'
-fi
-if grep -q 'UP-APL03' /sys/class/dmi/id/board_name
-then
-    #check blacklist
-    if grep -q 'blacklist gpio_aaeon' /etc/modprobe.d/blacklist.conf
-    then
-        echo "blacklist gpio_aaeon exist"
-    else
-        echo "blacklist gpio_aaeon" >> /etc/modprobe.d/blacklist.conf
-    fi
 fi
 
 if grep -q 'UP-ADLN01' /sys/class/dmi/id/board_name
